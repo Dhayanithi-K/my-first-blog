c407d7d051c53c226c23dd15297890939810f99a
Dhaya <dhaya07@hotmail.com>
2018-10-18 23:50:50 +0530

    Changed the html for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def path_list(request):
+	response=HttpResponse('<div style="color:red"><a href="www.google.com">Hello Welcome to my view</div>')
+	return render(request,'blog/post_list.html',{})
