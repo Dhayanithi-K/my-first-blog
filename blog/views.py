4cd20a4a14671729f4dd8df0daeb7f82ef06b91d
Dhaya <dhaya07@hotmail.com>
2018-10-19 16:02:43 +0530

    Changed html and views to display model data

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 from django.http import HttpResponse
 
 # Create your views here.
 
 def path_list(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	response=HttpResponse('<div style="color:red"><a href="www.google.com">Hello Welcome to my view</div>')
-	return render(request,'blog/post_list.html',{})
+	return render(request,'blog/post_list.html',{'posts':posts})
